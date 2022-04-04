7cc60b42884516aac80b460282c515a5e7cae85d
solparrondo <101563449+solparrondo@users.noreply.github.com>
2022-04-03 21:18:31 -0300

    First three videos completed
    Scenes folder created, script created and completed, Debug.Log and If-else sentence used, UI element created and modified its properties, Text linked with code.

---
+++ Assets/Scripts/MayoriaDeEdad.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayoriaDeEdad : MonoBehaviour
+{
+    public int edadUsuario;
+    public Text miTexto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (edadUsuario >= 18)
+        {
+            miTexto.text = "Es mayor de edad";
+            Debug.Log("Es mayor de edad");
+        }
+        else 
+        {
+            miTexto.text = "No es mayor de edad";
+            Debug.Log("No es mayor de edad");
+        }
+           
+    }
+
+    
+}
